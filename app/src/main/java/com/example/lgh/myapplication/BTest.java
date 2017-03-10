f14e0f85e01c947a922f4bced4c39bb2b934a48f
guanghui liu <ghliu@mobvoi.com>
2017-03-10 11:05:53 +0800

    test

---
+++ app/src/main/java/com/example/lgh/myapplication/BTest.java
@@ -0,0 +1,19 @@
+package com.example.lgh.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lgh on 2017/2/22.
+ */
+
+public class BTest extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_b);
+        MediaSessionCompat c;
+    }
+}
